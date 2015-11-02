bd9e9f751c47790f02d9dc1db39ef45aa00e8d4d
chscqe <christoph.r.schmdt@fau.de>
2015-11-02 10:05:43 +0100

    Made changes in the Coordinate.java as requested

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -10,19 +10,24 @@ public class Coordinate {
 		longitude = longi;
 	}
 
-	public Coordinate getDistance(Coordinate other) {
+	public double getDistance(Coordinate other) {
 		if(other == null) {
 			throw new IllegalArgumentException();
 		}
-		return new Coordinate(Math.abs(getLatitudinalDistance(other)), Math.abs(getLongitudinalDistance(other)));
+		double deltaX = Math.cos(other.getLongitude())*Math.cos(other.getLatitude()) - Math.cos(getLongitude()) * Math.cos(getLatitude());
+		double deltaY = Math.cos(other.getLongitude())*Math.sin(other.getLatitude()) - Math.cos(getLongitude()) * Math.sin(getLatitude());
+		double deltaZ = Math.sin(other.getLongitude()) - Math.sin(getLongitude());
+		double C = Math.sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+		double deltaSig = 2.0 * Math.asin(C / 2.0);
+		return 6371.0 * deltaSig;
 	}
 	
 	public double getLatitudinalDistance(Coordinate other)	{
-		return Math.abs(latitude - other.latitude);
+		return Math.abs(latitude - other.getLatitude());
 	}
 	
 	public double getLongitudinalDistance(Coordinate other) {
-		return Math.abs(longitude - other.longitude);
+		return Math.abs(longitude - other.getLongitude());
 	}
 	
 	public double getLongitude() {
