a769e402855ea9aa247f6ff54ca3af66ff403330
chscqe <christoph.r.schmdt@fau.de>
2015-11-02 10:08:50 +0100

    Added Location.java

---
+++ src/main/java/org/wahlzeit/model/Location.java
@@ -0,0 +1,17 @@
+package org.wahlzeit.model;
+
+public class Location {
+	
+	private String name;
+
+	public Coordinate coordinate;
+	
+	public Location() {
+		
+	}
+	
+	public Location(String name) {
+		this.name = name;
+	}
+
+}
