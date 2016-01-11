3b15c721c56c1059a9d36270d36fc507667f82cd
chscqe <christoph.r.schmdt@fau.de>
2016-01-11 11:44:25 +0100

    Implemented WindowType.java

--- src/main/java/org/wahlzeit/model/WindowType.java
+++ src/main/java/org/wahlzeit/model/WindowType.java
@@ -4,4 +4,32 @@ import java.io.Serializable;
 
 public class WindowType implements Serializable{
 
+	private final String windowType;
+	private final WindowType superType;
+	
+	public WindowType(String windowType, WindowType superType) {
+		this.windowType = windowType;
+		this.superType = superType;
+	}
+	
+	/**
+	 * @methodtype construct
+	 */
+	public Window createInstance() {
+		return new Window(this);
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public WindowType getSuperType() {
+		return superType;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public String getWindowTypeName() {
+		return windowType;
+	}
 }
