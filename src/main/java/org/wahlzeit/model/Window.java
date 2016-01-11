63d04d5d316b6017ce711f474314159349366570
chscqe <christoph.r.schmdt@fau.de>
2016-01-11 11:39:45 +0100

    Implemented Window.java with its getters and setters

--- src/main/java/org/wahlzeit/model/Window.java
+++ src/main/java/org/wahlzeit/model/Window.java
@@ -5,4 +5,67 @@ import java.io.Serializable;
 public class Window implements Serializable{
 
 	private WindowType windowType;
+	private String building;
+	private Location location;
+	private String epoch;
+	
+	public Window(WindowType windowType) {
+		this.windowType = windowType;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public WindowType getWindowType() {
+		return windowType;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public String getBuilding() {
+		return building;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public Location getLocation() {
+		return location;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public String getEpoch() {
+		return epoch;
+	}
+	
+	/**
+	 * @methodtype set
+	 */
+	public void setWindowType(WindowType w) {
+		this.windowType = w;
+	}
+	
+	/**
+	 * @methodtype set
+	 */
+	public void setBuilding(String s) {
+		this.building = s;
+	}
+	
+	/**
+	 * @methodtype set
+	 */
+	public void setLocation(Location l) {
+		this.location = l;
+	}
+	
+	/**
+	 * @methodtype set
+	 */
+	public void setEpoch(String s) {
+		this.epoch = s;
+	}
 }
